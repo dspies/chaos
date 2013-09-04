c7dbf3ed533264f5133fb3892978267dafc4044a
David Spies <david.m.spies@gmail.com>
2013-09-03 21:33:35 -0700

    Added scaffolding templates with EqualsAndHashCode to domain classes

---
+++ src/templates/artifacts/DomainClass.groovy
@@ -0,0 +1,18 @@
+@artifact.package@
+import groovy.transform.EqualsAndHashCode
+
+/**
+ * Brief description
+ * @author your name
+ */
+@EqualsAndHashCode
+class @artifact.name@ {
+
+    static constraints = {
+
+    }
+
+    String toString(){
+        super.toString();
+    }
+}
