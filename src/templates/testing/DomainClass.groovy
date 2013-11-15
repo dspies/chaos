2ad5b96bdc5753a3f86afcbe4ff76ef32b1f4a6c
David Spies <david.m.spies@gmail.com>
2013-11-15 09:56:24 -0700

    Changed default test template to Spock Specification
    Changed the src/templates/testing/DomainClass.groovy to use Spock specifications.  Due to limitations in Grails scaffolding, the artifact name was used as the variable/instance variable for the tests.  This will need to be changed in each test that is auto-generated.

--- src/templates/testing/DomainClass.groovy
+++ src/templates/testing/DomainClass.groovy
@@ -2,14 +2,29 @@
 
 import grails.test.mixin.*
 import org.junit.*
+import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(@artifact.testclass@)
-class @artifact.name@ {
+class @artifact.name@ extends Specification {
 
-    void testSomething() {
-       fail "Implement me"
+    void "(fieldName) constraint - #errorMessage"() {
+        setup:
+            mockForConstraintsTests(@artifact.testclass@)
+
+        when:
+            //def @artifact.testclass@ = new @artifact.testclass@()
+
+        then:
+            fail "Implement me"
+            //@artifact.testclass@.validate() == valid
+            //@artifact.testclass@.errors['(fieldName)'] == errorMessage
+
+        where:
+            //(fieldName)  | valid | errorMessage
+            //(fieldValue) | false | 'error message, such as "nullable"'  //Cannot be ...
+            //(fieldValue) | true  | null     //Valid
     }
 }
